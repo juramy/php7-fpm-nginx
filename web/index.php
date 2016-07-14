2d2da69c8edfce944af05b92e4ba57930a8fb2be
Tom Imrei <tom@balr.com>
2016-07-14 09:46:25 +0200

    Initial commit, first working version

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+    <h1><?php echo "Hello World!"; ?></h1>
+    <p>If you can see this page, your server is running.</p>
+    </body>
+</html>
